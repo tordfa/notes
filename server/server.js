0181d77bb2c636220ef0ad1f6e5b48ad2cd23bda
TorFa <tordfalch@hotmail.com>
2022-10-11 12:54:35 +0200

    ..

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     process.exit();
   })
 
-app.use(cors(({origin: 'http://localhost:3000', credentials: true})))
+app.use(cors(({credentials: true})))
 app.use(cookieParser());
 app.use(express.json())
 
